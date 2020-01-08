590ddb6383ebe3b97f0825f961949b7838c8fe32
yhbao <yhbao@yhbit.net>
2020-01-08 18:01:28 +0800

    refactor(plugin-webpack): export some helpers

--- packages/plugin-webpack/src/index.ts
+++ packages/plugin-webpack/src/index.ts
@@ -1,7 +1,10 @@
 export { default } from "./plugins/webpack";
+export { default as buildExtensionsRegexp } from "./utils/build-extensions-regexp";
+export { default as normalizeExtension } from "./utils/normalize-extension";
 export { default as useBabelLoader } from "./utils/use-babel-loader";
 export { default as useCssLoader } from "./utils/use-css-loader";
 export { default as useFileLoader } from "./utils/use-file-loader";
 export { default as useLessLoader } from "./utils/use-less-loader";
 export { default as usePostcssLoader } from "./utils/use-postcss-loader";
 export { default as useStyleLoader } from "./utils/use-style-loader";
+export { default as useUrlLoader } from "./utils/use-url-loader";
