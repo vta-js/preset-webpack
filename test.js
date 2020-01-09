0ba8877c385751d9b4b68339a8709e43d330535f
yhbao <yhbao@yhbit.net>
2020-01-09 10:26:19 +0800

    test(plugin-webpack): move plugin-test to __tests__

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ const { run } = require("vta"); // eslint-disable-line
 
 const isDev = true;
 
-process.chdir(path.resolve(__dirname, "./packages/plugin-webpack/plugin-test"));
+process.chdir(
+  path.resolve(__dirname, "./packages/plugin-webpack/__tests__/data/project/project-01"),
+);
 process.env.NODE_ENV = isDev ? "development" : "production";
 process.env.VTA_ENV = isDev ? "development" : "production";
 run();
